24f4c0210a630e851201f1dea43b8af2574f84a1
Irene Fung <irene.ifung@gmail.com>
2015-08-12 10:06:27 -0700

    Testing cookies on server

--- controllers/home.js
+++ controllers/home.js
@@ -5,6 +5,8 @@ var User = require('../models/User');
  * Home page.
  */
 exports.index = function(req, res) {
+  console.log("Cookies: ", req.cookies);
+  
   if (req.user) {
     User.findById(req.user.id, function(err, user) {
       res.render('journal', {
