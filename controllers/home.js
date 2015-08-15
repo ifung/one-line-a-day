01bd9c81dfb70570469084844258de2641705fe1
Irene Fung <irene.ifung@gmail.com>
2015-08-14 17:29:16 -0700

    Bug fix

--- controllers/home.js
+++ controllers/home.js
@@ -1,11 +1,29 @@
 var User = require('../models/User');
+var moment = require('moment');
 
 /**
  * GET /
  * Home page.
  */
 exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  })
+  var timezone = req.cookies.timezone;
+  var currTz = moment().tz(timezone);
+  
+  if (req.user) {
+    User.findById(req.user.id, function(err, user) {
+      res.render('journal', {
+        title: 'Journal',
+        timezone: req.cookies.timezone,
+        journal: user.journal[currTz.format('MMDD')],
+        month: currTz.format('MMMM'),
+        day: currTz.format('D')
+      })
+    });
+  }
+  else
+  {
+    res.render('home', {
+      title: 'Home'
+    })
+  }
 };
